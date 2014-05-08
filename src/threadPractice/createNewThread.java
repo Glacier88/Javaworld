418264cd0ff2829c2ce71886c3f22d72f54f73a9
Glocier <4silentliao@gmail.com>
2014-05-08 15:26:07 +0800

    ThreadPractice

--- src/threadPractice/createNewThread.java
+++ src/threadPractice/createNewThread.java
@@ -1,9 +1,13 @@
 package threadPractice;
 
 class newThread implements Runnable{
-	private Thread t;
-	newThread(){
+    Thread t;
+	private String threadName;
+	
+	newThread(String s){
 		t=new Thread(this,"Child thread");
+		t.setName(s);
+		threadName=s;
 		System.out.println("child thread"+t);
 		t.start();
 	}
@@ -11,7 +15,7 @@ class newThread implements Runnable{
 		for(int i=1;i<6;i++){
 			String temp=Integer.toString(i);
 			String temp2=temp+temp;
-			System.out.println(temp2);
+			System.out.println(threadName+":"+temp2);
 			try{
 			Thread.sleep(500);
 			}catch(InterruptedException e){
@@ -20,10 +24,41 @@ class newThread implements Runnable{
 		}
 	}
 }
-public class createNewThread {
-	public static void main(String args[]){
-		new newThread();
+
+class ExtendNewThread extends Thread{
+	private String threadName;
+	Thread t;
+	
+	ExtendNewThread(String s){
+		//super(s);
+		t=new Thread(s);
+		threadName=s;
+		System.out.println("This thread"+this);
+		start();
+	}
+	
+	public void Runnable(){
+		for(int i=115;i<121;i++){
+			System.out.println(threadName+":"+i);
+			try{
+				Thread.sleep(500);
+			}catch(InterruptedException e){
+				System.out.println("Thread Interrupted");
+			}
+		}
+	}
+}
+
+public class createNewThread{
+	public static void main(String args[]) throws InterruptedException{
+		newThread Athread=new newThread("one");
+		ExtendNewThread extendThread=new ExtendNewThread("two");
+		extendThread.Runnable();
+		Athread.t.join();
+		extendThread.t.join();
 		System.out.println(Thread.currentThread());
+		
+		
 		
 		for(int i=1;i<6;i++){
 			System.out.println(i);

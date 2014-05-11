20de1712e4321b7a0ba66c71ac1eeb03ea45f54e
Glocier <4silentliao@gmail.com>
2014-05-12 07:43:58 +0800

    finish binary search in array

--- src/dataStructures/ArrayBinarySearch.java
+++ src/dataStructures/ArrayBinarySearch.java
@@ -25,10 +25,37 @@ public class ArrayBinarySearch {
 		}
 	}
 	
+	public static String findTemplate(long[] array,long searchKey){
+		int lowerBound=0;
+		int upperBound=array.length-1;
+		String status=new String();
+		while(true){
+			int mid=(lowerBound+upperBound)/2;
+			if(array[mid]==searchKey){
+				status="Found";
+				break;
+			}
+			else if(lowerBound>upperBound){
+				status="Not found";
+				break;
+			}
+			else if(array[mid]>searchKey){
+				upperBound-=1;
+			}
+			else{
+				lowerBound+=1;
+			}
+		}
+		return status;
+	}
+	
 	public static void main(String args[]){
 		long[] array={2,3,4,5,7,8,9,10,11,12,13,15};
 		String status;
+		String status2;
 		status=find(array,888);
+		status2=findTemplate(array,8);
 		System.out.println(status);
+		System.out.println(status2);
 	}
 }

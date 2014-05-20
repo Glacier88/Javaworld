1a47bb5ae1d3ce2a5374a8f772820b0243becc87
Glocier <4silentliao@gmail.com>
2014-05-20 08:39:54 +0800

    Queue finished

--- src/dataStructureLinkList/LinkQueueApp.java
+++ src/dataStructureLinkList/LinkQueueApp.java
@@ -20,8 +20,10 @@ class LinkListQ{
 	void insertLast(int data){
 		LinkQ link=new LinkQ(data);
 		if(first==null)
-			last=link;
-		link.next=first;
+			first=link;
+		else 
+			last.next=link;
+		last=link;
 	}
 	
 	int deleteFirst(){
@@ -29,8 +31,48 @@ class LinkListQ{
 		first=first.next;
 		return temp;
 	}
+	
+	void displayList(){
+		LinkQ current=first;
+		while(current!=null){
+			current.display();
+			current=current.next;
+		}
+	}
+}
+
+class Queue{
+	LinkListQ list=new LinkListQ();
+	
+	void insert(int data){
+		list.insertLast(data);
+	}
+	
+	void remove(){
+		list.deleteFirst();
+	}
+	
+	void displayQueue(){
+		list.displayList();
+	}
 }
 
 public class LinkQueueApp {
-
+	public static void main(String args[]){
+		Queue queue=new Queue();
+		queue.insert(2);
+		queue.insert(5);
+		queue.insert(7);
+		queue.insert(9);
+		queue.insert(10);
+		System.out.println("Queue:from front to rear");
+		queue.displayQueue();
+		queue.remove();
+		System.out.println("After a remove");
+		queue.displayQueue();
+		queue.insert(888);
+		System.out.println("After an insertion");
+		queue.displayQueue();
+		
+	}
 }
